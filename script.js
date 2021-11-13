9f674eeb08cf213c7d2323e1684b97226b8ad8ef
pallabez <pallabsonowal999@gmail.com>
2021-11-13 18:28:21 +0530

    Animated skill bars

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict"
 
+
+//Smooth Scroolling
 var nav = document.getElementsByClassName('nav-link');
 
 for(let i of nav) {
@@ -18,4 +20,43 @@ for(let i of nav) {
         }, 2);
     });
 }
+
+
+//Skill animation
+var skills = document.getElementById('skills');
+var skillDisplayed = false;
+
+function isVisible(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top <= window.innerHeight;
+}
+
+function checkScroll() {
+    var skillBars = document.querySelectorAll(".skill-display div");
+    var skillVisibility = isVisible(skills);
+
+    if(skillVisibility && !skillDisplayed) {
+        skillDisplayed = true;
+        for(let i of skillBars) {
+            var width = i.getAttribute("data-value");
+            changeWidth(0, width, i);
+        }
+    } else if(!skillVisibility && skillDisplayed) {
+        skillDisplayed = false;
+        for(let i of skillBars) {
+            i.style.width = "0%";
+        }
+    }
+}
+
+function changeWidth(initial, final, element) {
+    let currWidth = Number(initial);
+
+    var interval = setInterval(function() {
+        element.style.width = (++currWidth) + "%";
+        if(currWidth >= final) clearInterval(interval);
+    }, 4);
+}
+window.addEventListener('scroll', checkScroll);
+
 
